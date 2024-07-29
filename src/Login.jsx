79a46a9757a28d24b472c481f69db88ec0a9bd3c
Santiago <santiago.garcia.2007.22@gmail.com>
2024-07-29 07:57:02 -0500

    TASK:003

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./index.css";
-import LogoInstagram from "./assets/logo-instagram.png";
 import Header from "./Header.jsx";
 import Login_s from "./Login_s.jsx";
 
---
+++ src/mocks/users.js
@@ -0,0 +1,22 @@
+export const cards = [
+    {
+      id: 1,
+      nombre: "Jhojan",
+      edad: 28,
+      username: "jhojan_garcia28",
+      password: "Root1",
+    },
+    {
+      id: 2,
+      nombre: "gomezlunaoscar",
+      edad: 88,
+      password: "Root2",
+    },
+    {
+      id: 3,
+      nombre: "santiagogarcia6966",
+      edad: 17,
+      password: "Root3",
+    },
+  ];
+  
