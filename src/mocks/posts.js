4945be81a6a65013c6aaa186531bda6edf447228
Santiago <santiago.garcia.2007.22@gmail.com>
2024-08-01 16:53:23 -0500

    task:008

--- src/mocks/posts.js
+++ src/mocks/posts.js
@@ -1,61 +1,52 @@
-import user from './users'
-
 export const posts = [
   {
     id: 1,
-    title:"this is a title",
+    title: "this is a title",
     image: "https://via.assets.so/album.png?id=1&q=95&w=360&h=360&fit=fill",
     description: "this is a descriptiion",
     likes:"2",
-    userId:1
+    userId: 1,
   },
   {
     id: 2,
-    title:"this is a title",
     image: "https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill",
     description: "this is a descriptiion",
     likes:"3",
-    userId:2
+    userId: 2,
   },
+ 
   {
     id: 3,
-    title:"this is a title",
     image: "https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill",
     description: "this is a descriptiion",
     likes:"4",
-    userId:3
+    userId: 3,
   },
     
   {
     id: 4,
-    title:"this is a title",
     image: "https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill",
     description: "this is a descriptiion",
     likes:"4",
-    userId:4
-
+    userId: 4,
+    
   },{
     id: 5,
-    title:"this is a title",
     image: "https://via.assets.so/watch.png?id=1&q=95&w=360&h=360&fit=fill",
     description: "this is a descriptiion",
     likes:"4",
-    userId:5
-
+    userId: 5,
   },{
     id: 6,
-    title:"this is a title",
     image: "https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill",
     description: "this is a descriptiion",
     likes:"2",
-    userId:6
-
+    userId: 6,
   },{
     id: 7,
-    title:"this is a title",
     image: "https://via.assets.so/shoe.png?id=1&q=95&w=360&h=360&fit=fill",
     description: "this is a descriptiion",
     likes:"6",
-    userId:2
+    userId: 7,
   },
 ];
